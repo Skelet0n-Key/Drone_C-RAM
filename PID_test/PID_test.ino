af12c03656955185b3753271d1175186eaf2852e
megabyte512 <shlolfwall@gmail.com>
2025-11-05 16:02:04 -0700

    updated pid_test

--- PID_test/PID_test.ino
+++ PID_test/PID_test.ino
@@ -1,15 +1,18 @@
-#include <SoftwareSerial.h>
-
 String coords = "";
 int x = 0;
 int y = 0;
 int centerX = 320;
 int centerY = 240;
 int xFreq = 0;
-uint8_t yFreq = 0;
+int yFreq = 0;
+
+int xPulsePin = 9;   // Timer1 OC1A
+int xDirPin = 8;
+int yPulsePin = 11;  // Timer2 OC2A
+int yDirPin = 10;
 
 void setup() {
-   // Timer1 - prescaler 8
+  // Timer1 - prescaler 8
   TCCR1A = (1 << COM1A0);
   TCCR1B = (1 << WGM12) | (1 << CS11);
   OCR1A = (F_CPU / (2 * 8 * 1000)) - 1;
@@ -18,20 +21,13 @@ void setup() {
   TCCR2A = (1 << COM2A0) | (1 << WGM21);
   TCCR2B = (1 << CS22);
   OCR2A = (uint8_t)((F_CPU / (2UL * 64UL * 1000UL)) - 1); // you wouldn't believe how stupid this integer overflow fix is
-
-  int yPulsePin = 8;
-  int yDirPin = 9;
-  int xPulsePin = 10;
-  int xDirPin = 11;
-
-  pinMode(yPulsePin, OUTPUT);
-  pinMode(yDirPin, OUTPUT);  // High is pitch up
-  pinMode(xPulsePin);
+  
+  pinMode(xPulsePin, OUTPUT);
   pinMode(xDirPin, OUTPUT);  // High is counter-clockwise, left
+  pinMode(yPulsePin, OUTPUT);
+  pinMode(yDirPin, OUTPUT);   // High is pitch up
 
-
-  SoftwareSerial rpiCoords(2, 3);
-  rpiCoords.begin(115200);
+  Serial.begin(115200);
 }
 
 // Function prototypes
@@ -41,11 +37,12 @@ void stopTimer2();
 void startTimer2();
 void setxFreq(int xFreq);
 void setyFreq(int yFreq);
-int frequency_calculator(int coord, int curr_freq);
+int frequency_calculator(int coord, int curr_freq, int center);
+void set_dir(char axis, int center, int coord);
 
 void loop() {
-  while (rpiCoords.available() > 0) {  // receiving coords
-    char c = rpiCoords.read();
+  while (Serial.available() > 0) {  // receiving coords
+    char c = Serial.read();
     if (c == '\n') {  // if we received full coords
       // parse coords
       int commaIndex = coords.indexOf(',');
@@ -53,12 +50,23 @@ void loop() {
       y = coords.substring(commaIndex + 1).toInt();
       // change direction with parsed coords
       set_dir('x', centerX, x);
-      set_dir('y', cetnerY, y);
+      set_dir('y', centerY, y);
       // change frequency with parsed coords
       xFreq = frequency_calculator(x, xFreq, centerX);
       yFreq = frequency_calculator(y, yFreq, centerY);
       setxFreq(xFreq);
-      setYFreq(yFreq);
+      setyFreq(yFreq);
+      // --------------------------------------
+      Serial.println("---------------------------------");
+      Serial.println("xCoord: " + String(x));
+      Serial.println("xFreq: " + String(xFreq));
+      Serial.println("yCoord: " + String(y));
+      Serial.println("yFreq: " + String(yFreq));
+
+      Serial.println("OCR1A = " + String((F_CPU / (2UL * 8UL * (unsigned long)xFreq)) - 1));
+      Serial.println("OCR2A = " + String((uint8_t)((F_CPU / (2UL * 64UL * (unsigned long)yFreq)) - 1)));
+      // --------------------------------------
+      coords = "";  // clear for next message
     } else coords += c;  // continue to append chars until '\n'
   }
   // timer frequencies cannot not go lower than 122Hz or higher than 1MHz
@@ -89,7 +97,7 @@ void startTimer2() {
 }
 
 void setxFreq(int xFreq) {
-  OCR1A = (F_CPU / (2 * 8 * xFreq)) - 1;
+  OCR1A = (F_CPU / (2UL * 8UL * (unsigned long)xFreq)) - 1;
 }
 
 void setyFreq(int yFreq) {
@@ -101,18 +109,14 @@ int frequency_calculator(int coord, int curr_freq, int center) {
   // a coord of -300 and +300 should return the same freq, we don't care abt dir
   if (curr_freq > 10*abs(coord-center)) {  // if approaching target too fast, slow down.
     if (10*abs(coord-center) < 122) {
-      return 122
+      return 122;
     }
-    return 10*abs(coord-center)
-  // } else if (curr_freq == 0) {  // if stopped, start slow, don't jerk.
-  //   return 1000
-  // } else if (coord < 20) {  // if close enough to target, stop
-  //   return 0
+    return 10*abs(coord-center);
   } else {  // if approaching target and still accelerating, continue to accelerate
     if (curr_freq*2 > 2000) {
-      return 2000
+      return 2000;
     }
-    return curr_freq*2
+    return curr_freq*2;
   }
   // all hardcoded values subject to change. Just an example for now.
 }
@@ -121,29 +125,24 @@ void set_dir(char axis, int center, int coord) {
   // x and y are on separate timers, we need to be able to distinguish here.
   // not possible to put this logic in the frequency calculator
   if (axis == 'x') {
-    if (coord-center > 10) {
+    if (coord - center > 10) {
       startTimer1();  // outside of deadzone, connect timer to pin
       digitalWrite(xDirPin, LOW);  // moving clockwise
-    } else if (coord-center < 10 && coord-center > -10) {
-      stopTimer1();  // inside of deadzone, disconnect timer
-      // don't care abt direction in this case
-    } else if (coord-center < -10) {
+    } else if (coord - center < -10) {
       startTimer1();  // outside of deadzone
       digitalWrite(xDirPin, HIGH);  // moving counter-clockwise
+    } else {
+      stopTimer1();  // inside of deadzone, disconnect timer
     }
   } else if (axis == 'y') {
-    if (coord-center > 10) {
+    if (coord - center > 10) {
       startTimer2();  // outside of deadzone, connect timer to pin
       digitalWrite(yDirPin, LOW);  // moving clockwise
-    } else if (coord-center < 10 && coord-center > -10) {
-      stopTimer2();  // inside of deadzone, disconnect timer
-      // don't care abt direction in this case
-    } else if (coord-center < -10) {
+    } else if (coord - center < -10) {
       startTimer2();  // outside of deadzone
       digitalWrite(yDirPin, HIGH);  // moving counter-clockwise
+    } else {
+      stopTimer2();  // inside of deadzone, disconnect timer
     }
   }
 }
-
-
-
